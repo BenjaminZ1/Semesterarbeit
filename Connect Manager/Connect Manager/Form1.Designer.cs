f262290c90152322fd20a3df8454a6987257608c
Benjamin Zech <benjamin.zech@ari-ag.ch>
2020-01-21 16:34:54 +0100

    Button wieder entfernt

--- Connect Manager/Connect Manager/Form1.Designer.cs
+++ Connect Manager/Connect Manager/Form1.Designer.cs
@@ -28,24 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(459, 84);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(110, 28);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(19F, 37F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(253, 158);
+            this.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -53,7 +43,6 @@
         }
 
         #endregion
-        private System.Windows.Forms.Button button2;
     }
 }
 
